9a81d100869425fb56dc000584ecd19f0ba70227
parkjuhee <rabitangl20@naver.com>
2025-05-13 20:13:17 +0900

    feat : UserRepository 생성 - #1

---
+++ src/main/java/org/mjulikelion/board/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package org.mjulikelion.board.repository;
+
+import org.mjulikelion.board.domain.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<UserEntity, Integer> {
+    //username존재 여부 반환
+    Boolean existsByUsername(String username);
+}
